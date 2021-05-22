c1c1954e51201d0670629d6f0315c85e065e2caa
Anubhav Omar <64382574+RKO4444@users.noreply.github.com>
2021-05-23 04:30:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+Cookies.set("cookie1", "1");
+var myCookie=Cookies.get("cookie1");
+
+
+if(myCookie==2)
+{
+document.querySelector("h1").innerHTML = "hello1";
+
+
+}
+if(myCookie==3)
+{
+document.querySelector("h1").innerHTML = "hello3";
+
+
+}
+if(myCookie==4)
+{
+document.querySelector("h1").innerHTML = "hello4";
+
+
+}
+if(myCookie==5)
+{
+document.querySelector("h1").innerHTML = "hello5";
+
+
+}
+if(myCookie==6)
+{
+document.querySelector("h1").innerHTML = "hello6";
+
+
+}
+if(myCookie==7)
+{
+document.querySelector("h1").innerHTML = "hello7";
+
+
+}
